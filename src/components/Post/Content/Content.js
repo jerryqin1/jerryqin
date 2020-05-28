fb2bf7aea375d8d38e8be8442fe090784f785dd1
Jerry Qin <jerry.gn.qin@gmail.com>
2020-05-28 02:34:34 -0700

    Update Content.js

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -4,11 +4,10 @@ import styles from './Content.module.scss';
 
 type Props = {
   body: string,
-  title: string,
-  subtitle: string
+  title: string
 };
 
-const Content = ({ body, title, subtitle }: Props) => (
+const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
