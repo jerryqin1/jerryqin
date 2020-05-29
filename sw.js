2a55960634fb560df15c948ed9ddf3a52d57c9f5
Jerry Qin <jerry.gn.qin@gmail.com>
2020-05-29 15:28:34 -0700

    Updates

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-6d4854340fffb1fd81d8.js"
   },
   {
-    "url": "styles.7bce9a571a3b691d02e9.css"
+    "url": "styles.dfe7032cacac1fa5ce60.css"
   },
   {
     "url": "styles-9b6f388623a2ec93d35f.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "55226941410528165f95ca6246efab98"
+    "revision": "68edd4536f6ed7d15669886d6ef3b601"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e8e7fbb006d57d32ed40.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9b54e49285d6593b60ee6dec72ae9062"
+    "revision": "bc8ee1e8b5666014f287308fac489f87"
   },
   {
     "url": "manifest.json",
